f2e287c92af5c82610040949d6e4715d40bc7f3e
bnguye40-uncc <bnguye40@charlotte.edu>
2025-04-24 04:14:32 -0400

    Working on IFolderItems interface for folder item lists. Completed Bookmark data structure.

--- FinalProject/Components/Pages/Objects/Bookmark.razor.cs
+++ FinalProject/Components/Pages/Objects/Bookmark.razor.cs
@@ -2,13 +2,20 @@ using Microsoft.AspNetCore.Components;
 
 namespace FinalProject.Components.Pages.Objects;
 
-public partial class Bookmark : ComponentBase
+public partial class Bookmark : ComponentBase, IFolderItem
 {
+    [Parameter]
     public string Name { get; set; } = "Default";
+    [Parameter]
     public string Description { get; set; } = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet quam at sem semper ultricies. Nunc orci justo, blandit non arcu at, auctor feugiat neque. Nunc vel dapibus leo, eget porttitor leo.";
+    [Parameter]
     public string URL { get; set; } = "https://docs.microsoft.com/aspnet/";
+    [Parameter]
     public string[] Tags { get; set; } = ["tag1", "tag2", "tag3"];
+    [Parameter]
     public string Keyword { get; set; } = "keyword";
+    [Parameter]
     public Template CustomTemplate { get; set; } = new Template();
+    [Parameter]
     public List<Field> CustomFieldsArray { get; set; } = new List<Field> { new Field(), new Field() };
 }
--- FinalProject/Components/Pages/Objects/Template.razor.cs
+++ FinalProject/Components/Pages/Objects/Template.razor.cs
@@ -4,7 +4,10 @@ namespace FinalProject.Components.Pages.Objects;
 
 public partial class Template : ComponentBase
 {
+    [Parameter]
     public string Name { get; set; } = "Default Template";
+    [Parameter]
     public string Description { get; set; } = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    [Parameter]
     public List<Field> TemplateFieldsArray { get; set; } = [];
 }
