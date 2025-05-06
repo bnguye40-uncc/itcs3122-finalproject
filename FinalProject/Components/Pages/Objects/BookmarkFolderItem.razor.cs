53cc89f52f353d81755b86ae93613f6e42d66a40
bnguye40-uncc <bnguye40@charlotte.edu>
2025-05-06 10:11:56 -0400

    Separated Bookmark into two components (Bookmark, BookmarkFolderItem).

--- FinalProject/Components/Pages/Objects/BookmarkFolderItem.razor.cs
+++ FinalProject/Components/Pages/Objects/BookmarkFolderItem.razor.cs
@@ -5,7 +5,7 @@ using FinalProject.Shared;
 namespace FinalProject.Components.Pages.Objects;
 
 [Serializable]
-public partial class Bookmark : AbstractFolderItem
+public partial class BookmarkFolderItem : AbstractFolderItem
 { 
     // Parameters
     [Parameter] public override string Class { get; set; } = "bookmark";
@@ -14,25 +14,14 @@ public partial class Bookmark : AbstractFolderItem
     [Parameter] public string URL { get; set; } = "https://docs.microsoft.com/aspnet/";
     [Parameter] public string Keyword { get; set; } = "keyword";
     [Parameter] public List<Field> CustomFields { get; set; } = new List<Field> { new Field(), new Field() };
-    [Parameter] public int SetHashCode { get; set; }        // Used for hashed routing
 
     // Component functions
-    protected override void OnParametersSet() {
-        var data = _stateContainer.GetRoutingObjectParameter<BookmarkFolderItem>(SetHashCode);
-        Name = data.Name;
-        Description = data.Description;
-        URL = data.URL;
-        Keyword = data.Keyword;
-        CustomFields = data.CustomFields;
+    protected override void OnInitialized()
+    {
+        _stateContainer.AddRoutingObjectParameter(this);
     }
 
-    private void AddField() {
-        CustomFields.Add(new Field());
-        _MainLayout.UpdateLocalStorage();
-    }
-
-    private void DeleteField(int index) {
-        CustomFields.Remove(CustomFields[index]);
-        _MainLayout.UpdateLocalStorage();
+    private void NavigateToBookmark() {
+        _navigationManager.NavigateTo($"bookmark/{this.GetHashCode()}");
     }
 }
