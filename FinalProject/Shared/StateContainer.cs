2810dc43d0dd6683bdac074db3150716a9fc6185
bnguye40-uncc <bnguye40@charlotte.edu>
2025-05-07 01:34:15 -0400

    Organizing

--- FinalProject/Shared/StateContainer.cs
+++ FinalProject/Shared/StateContainer.cs
@@ -5,20 +5,4 @@ namespace FinalProject.Shared;
 public class StateContainer
 {
     public readonly Dictionary<int, object> ObjectTunnel = new();
-    
-    private string? savedString;
-
-    public string Property
-    {
-        get => savedString ?? string.Empty;
-        set
-        {
-            savedString = value;
-            NotifyStateChanged();
-        }
-    }
-
-    public event Action? OnChange;
-
-    private void NotifyStateChanged() => OnChange?.Invoke();
 }
